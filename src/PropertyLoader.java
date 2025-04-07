06054b5b6f30fab7bfdfcae34db67cffd04e3447
史鲁平 <53071081+s-luping@users.noreply.github.com>
2025-04-07 15:42:15 +0800

    Update PropertyLoader.java

--- src/PropertyLoader.java
+++ src/PropertyLoader.java
@@ -1,42 +1,58 @@
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class PropertyLoader {
+    private static final Logger logger = Logger.getLogger(PropertyLoader.class.getName());
 
     /**
      * 
      * 从当前的类加载器的getResourcesAsStream来获取
      * InputStream inputStream =
      * this.getClass().getClassLoader().getResourceAsStream(name)
+     * 
+     * @throws Exception
      *
-     * @throws IOException
      */
-    public  Properties getProperties() throws IOException {
-        InputStream inputStream = this.getClass().getResourceAsStream("config.properties");
+
+    public Properties ETLConfig() {
         Properties properties = new Properties();
-        properties.load(inputStream);
+        try {
+            FileInputStream inputStream = new FileInputStream("config/config.properties");
+            properties.load(inputStream);
+        } catch (IOException e) {
+            System.out.println("获取配置文件失败");
+            e.printStackTrace();
+        }
+        logger.info("获取配置文件");
         return properties;
     }
-    public String getDimensionString() throws IOException {
-        Properties properties = getProperties();
-        String rawDimName = properties.getProperty("ess_dimensions");
-        String newDimName = "";
-        for (String split : rawDimName.split(",")) {
-            newDimName = newDimName + "\"" + split + "\"" + ",";
-            // String.join(str, "\""+split+"\"");
-        };
-        return newDimName.substring(0, newDimName.length()-1);
+
+    public String getDimensionString() {
+        Properties properties = ETLConfig();
+        String HPDims = properties.getProperty("ess_dimensions");
+        String Processed_HPDims = "";
+        for (String split : HPDims.split(",")) {
+            Processed_HPDims = Processed_HPDims + split + ",";
+        }
+        ;
+        Processed_HPDims = Processed_HPDims.substring(0, Processed_HPDims.length() - 1);
+        logger.info("获取维度查询字符串:" + Processed_HPDims);
+        return Processed_HPDims;
     }
-    public String getDimensionMapping() throws IOException {
-        Properties properties = getProperties();
-        String rawDimName = properties.getProperty("ess_dimensions");
-        String newDimName = "";
-        for (String split : rawDimName.split(",")) {
-            newDimName = newDimName + split + "=" + split + ",";
-            // String.join(str, "\""+split+"\"");
-        };
-        return newDimName.substring(0, newDimName.length()-1);
+
+    public String getDimensionMapping() {
+        String HPDims_Mapping = "";
+        Properties properties = ETLConfig();
+        String HPDims = properties.getProperty("ess_dimensions");
+        for (String split : HPDims.split(",")) {
+            HPDims_Mapping = HPDims_Mapping + split + "=" + split + ",";
+        }
+        ;
+        HPDims_Mapping = HPDims_Mapping.substring(0, HPDims_Mapping.length() - 1);
+        logger.info("获取维度映射:" + HPDims_Mapping);
+        return HPDims_Mapping;
     }
 
 }
