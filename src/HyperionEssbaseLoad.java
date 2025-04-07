7518b995079338b92f4e2f98360083d29d6e4700
史鲁平 <53071081+s-luping@users.noreply.github.com>
2025-04-07 15:41:22 +0800

    Update HyperionEssbaseLoad.java

--- src/HyperionEssbaseLoad.java
+++ src/HyperionEssbaseLoad.java
@@ -4,12 +4,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import com.hyperion.odi.common.ODIConstants;
 import com.hyperion.odi.common.ODIStatistics;
 import com.hyperion.odi.connection.HypAppConnectionFactory;
 import com.hyperion.odi.connection.IHypAppWriter;
+
 /**
  * 类 LoadDataToEssbase 用于加载数据到 Essbase 应用。
  */
@@ -25,16 +27,17 @@ public class HyperionEssbaseLoad {
             logger.info("开始 ------------------------");
             // 获取 Essbase 连接信息
             HashMap<String, Object> essProps = new HashMap<>();
-            essProps = ConnProperties.getEssbaseProp();
+            essProps = ConnProperties.EssbaseConnection();
             essProps.put(ODIConstants.WRITER_TYPE, "DATA_WRITER");
 
             logger.info("获取 Essbase 连接信息");
 
-            // 获取数据库连接
-            Connection srcProps = ConnProperties.getSrcDatabaseProp();
+            // 获取数据库连接和查询信息
+            HashMap<String, Object> srcProps = ConnProperties.getSrcDatabaseProp(args);
             logger.info("获取 数据库 连接信息");
 
-            IHypAppWriter essbaseAppWriter = HypAppConnectionFactory.getAppWriter(HypAppConnectionFactory.APP_ESSBASE, essProps);
+            IHypAppWriter essbaseAppWriter = HypAppConnectionFactory.getAppWriter(HypAppConnectionFactory.APP_ESSBASE,
+                    essProps);
             logger.info("初始化 Essbase 应用写入器并建立连接");
 
             // 加载数据
@@ -42,8 +45,8 @@ public class HyperionEssbaseLoad {
             loadOptions = getLoadOptions();
             essbaseAppWriter.beginLoad(loadOptions);
             logger.info("获取加载选项");
-            
-            String loadString = loadData(srcProps, essbaseAppWriter,args);
+
+            String loadString = loadData(srcProps, essbaseAppWriter, args);
             logger.info(loadString);
         } catch (Exception e) {
             logger.severe("异常: " + e.getMessage());
@@ -55,27 +58,30 @@ public class HyperionEssbaseLoad {
     /**
      * 从数据库加载数据并写入 Essbase。
      *
-     * @param srcConn 数据库连接
+     * @param srcConn          数据库连接
      * @param essbaseAppWriter Essbase 应用写入器
      * @return 加载状态信息
-     * @throws Exception 
+     * @throws Exception
      */
-    public static String loadData(Connection srcConn, IHypAppWriter essbaseAppWriter,String args[]) throws Exception {
+    public static String loadData(HashMap<String, Object> srcProps, IHypAppWriter essbaseAppWriter, String args[])
+            throws Exception {
         // 获取查询sql
-        String sqlData = queryString(args);
-        System.out.println(sqlData);
+        String querySQL = srcProps.get("querysql").toString();
+        System.out.println(querySQL);
+        Connection conn = (Connection) srcProps.get("conn");
         logger.info("开始加载数据 ------------------------");
         ODIStatistics status = new ODIStatistics();
-        try  {
-            Statement stmt2 = srcConn.createStatement();
-            ResultSet rs2 = stmt2.executeQuery(sqlData);
+        try {
+            Statement stmt2 = conn.createStatement();
             stmt2.setFetchSize(1);
+            ResultSet rs2 = stmt2.executeQuery(querySQL);
+            // ResultSet rs1 = stmt2.executeQuery(sql_Missing);
             // 假设这里有将结果集加载到Essbase的逻辑
+            // status = essbaseAppWriter.loadData(rs1);
             status = essbaseAppWriter.loadData(rs2);
-            System.out.printf("", rs2.getMetaData());;
             essbaseAppWriter.endLoad();
             rs2.close();
-            stmt2.close() ;
+            stmt2.close();
         } catch (SQLException e) {
             logger.severe("SQL异常: " + e.getMessage());
             throw new SQLException(e); // 重新抛出异常以便调用者能够处理
@@ -83,61 +89,33 @@ public class HyperionEssbaseLoad {
         return status.toString();
     }
 
-    public static String queryString(String args[]) throws Exception {
-        // 获取命令行参数
-        if (args.length != 4) {
-            logger.severe("ERROR:Missing parameter");
-            throw new Exception("Usage: java -jar HyperionEssbaseLoad.jar <operation_type> <batch_name> <year_num> <period_num>");
-        }
-        // 打印参数
-        System.out.println("Batch Name: " + args[1]);
-        System.out.println("Year Num: " + args[2]);
-        System.out.println("Period Num: " + args[3]);
-    
-        // 从配置文件获取维度列和数据表名称
-        PropertyLoader ploader = new PropertyLoader();
-        String columns = ploader.getDimensionString();
-        String srcTable = ploader.getProperties().getProperty("src_db_table");
-        StringBuilder sb = new StringBuilder();
-        
-        // 定义 SQL 语句用于从数据库加载数据
-        //"'#Missing' AS \"Data\"
-        sb.append("SELECT ");
-        sb.append(columns);
-        sb.append(" FROM ");
-        sb.append(srcTable);
-        sb.append(" WHERE (1=1) ");
-        sb.append("AND (BATCH_NAME='"+args[1]+"' ");
-        sb.append("AND YEAR_NUM='"+args[2]+"' ");
-        sb.append("AND PERIOD_NUM='"+args[1]+"') ");
-        return sb.toString();
-
-    }    /**
+    /**
      * 获取加载选项。
      *
      * @return 加载选项的 HashMap
      */
-    public static HashMap<String, Object> getLoadOptions(){
-        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd_hh_mm_ss");
+    public static HashMap<String, Object> getLoadOptions() {
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss");
+        Properties prop = new PropertyLoader().ETLConfig();
         // 定义并设置加载选项
-        String rulesFile = "ESS_LOAD" ;
-        String ruleSeparator = "," ;
-        String clearDatabase = "None" ;
-        String calcScript = "" ;
-        int maxErrors =  1;
-        String errFileName = "./ESS_LOAD_"+ft.format(new java.util.Date())+".err";
-        String logFileName = "./ESS_LOAD_"+ft.format(new java.util.Date())+".log";
+        String rulesFile = prop.getProperty("ess_load_rule");
+        String ruleSeparator = ",";
+        String clearDatabase = "None";
+        String calcScript = "";
+        int maxErrors = 1;
+        String errFileName = "../logs/" + prop.getProperty("ess_load_rule") + ft.format(new java.util.Date()) + ".err";
+        String logFileName = "../logs/" + prop.getProperty("ess_load_rule") + ft.format(new java.util.Date()) + ".log";
         String errColDelimiter = ",";
         String errRowDelimiter = "\r\n";
         String errTextDelimiter = "'";
         int commitInterval = 1000;
-        String preMaxlScript = "" ;
-        String postMaxlScript = "" ;
+        String preMaxlScript = "";
+        String postMaxlScript = "";
         int bufferId = 1;
         int bufferSize = 80;
         int groupId = 1;
 
-        HashMap<String,Object> loadOptions = new HashMap<>();
+        HashMap<String, Object> loadOptions = new HashMap<>();
         // 设置加载选项
         loadOptions.put(ODIConstants.CLEAR_DATABASE, clearDatabase);
         loadOptions.put(ODIConstants.CALCULATION_SCRIPT, calcScript);
@@ -153,15 +131,15 @@ public class HyperionEssbaseLoad {
         loadOptions.put(ODIConstants.ERR_TEXT_DELIMITER, errTextDelimiter);
         loadOptions.put(ODIConstants.ERR_LOG_HEADER_ROW, false);
         loadOptions.put(ODIConstants.COMMIT_INTERVAL, commitInterval);
-        loadOptions.put(ODIConstants.RUN_CALC_SCRIPT_ONLY,false);
-        loadOptions.put(ODIConstants.PRE_LOAD_MAXL_SCRIPT,preMaxlScript);
-        loadOptions.put(ODIConstants.POST_LOAD_MAXL_SCRIPT,postMaxlScript);
-        loadOptions.put(ODIConstants.ABORT_ON_PRE_MAXL_ERROR,false);
-        loadOptions.put(ODIConstants.BUFFER_SIZE,bufferSize);
-        loadOptions.put(ODIConstants.BUFFER_ID,bufferId);
-        loadOptions.put(ODIConstants.GROUP_ID,groupId);
+        loadOptions.put(ODIConstants.RUN_CALC_SCRIPT_ONLY, false);
+        loadOptions.put(ODIConstants.PRE_LOAD_MAXL_SCRIPT, preMaxlScript);
+        loadOptions.put(ODIConstants.POST_LOAD_MAXL_SCRIPT, postMaxlScript);
+        loadOptions.put(ODIConstants.ABORT_ON_PRE_MAXL_ERROR, false);
+        loadOptions.put(ODIConstants.BUFFER_SIZE, bufferSize);
+        loadOptions.put(ODIConstants.BUFFER_ID, bufferId);
+        loadOptions.put(ODIConstants.GROUP_ID, groupId);
         loadOptions.put(ODIConstants.UNICODE_IN_DATA, false);
         return loadOptions;
     }
-    
+
 }
